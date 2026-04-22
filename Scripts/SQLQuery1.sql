07c0199a0d19de6c3a979f42445958027f8ea237
Laythan Carelse <laythancarelse222@gmail.com>
2026-04-22 13:25:08 +0200

    We is making changes

---
+++ Scripts/SQLQuery1.sql
@@ -0,0 +1,9 @@
+INSERT INTO provinces (province_name) VALUES ('Gauteng');
+INSERT INTO provinces (province_name) VALUES ('Western Cape');
+INSERT INTO provinces (province_name) VALUES ('KwaZulu-Natal');
+INSERT INTO provinces (province_name) VALUES ('Limpopo');
+INSERT INTO provinces (province_name) VALUES ('Mpumalanga');
+INSERT INTO provinces (province_name) VALUES ('Free State');
+INSERT INTO provinces (province_name) VALUES ('Eastern Cape');
+INSERT INTO provinces (province_name) VALUES ('Northern Cape');
+INSERT INTO provinces (province_name) VALUES ('North West');
---
+++ Scripts/SQLQuery4.sql
@@ -0,0 +1,3 @@
+SELECT TOP (1000) [province_id]
+      ,[province_name]
+  FROM [Farmer_market_db].[dbo].[provinces]
